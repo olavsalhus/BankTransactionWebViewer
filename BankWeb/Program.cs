c8205d150b65786495eb33815ca90b74209ea067
Olav Salhus <osa@live.no>
2022-07-14 14:03:58 +0200

    Moved service to Program.cs

--- BankWeb/Program.cs
+++ BankWeb/Program.cs
@@ -1,25 +1,29 @@
 using System.Text.Json.Nodes;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient().AddSingleton<NordigenService>().AddRazorPages();
+builder.Services.AddHttpClient().AddRazorPages();
 
 var app = builder.Build();
 
 #region Minimal API
-app.MapGet("GetAccessToken", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService) =>
-    await ctx.Response.WriteAsync(await _nordigenService.GetAccessToken(httpClientFactory))
+app.MapGet("GetAccessToken", async (HttpContext ctx, IHttpClientFactory httpClientFactory, IConfiguration configuration) =>
+    await ctx.Response.WriteAsync(await GetAccessToken(httpClientFactory, configuration))
 );
 
-app.MapGet("GetBanks", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService, string? country, string? access_token) =>
+app.MapGet("GetBanks", async (HttpContext ctx, IHttpClientFactory httpClientFactory, IConfiguration configuration, string? country, string? access_token) =>
     await ctx.Response.WriteAsync(await Get($"institutions/?country={country}", access_token!, httpClientFactory))
 );
 
-app.MapGet("BankLogin", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService, string? bank) =>
+app.MapGet("BankLogin", async (HttpContext ctx, IHttpClientFactory httpClientFactory, IConfiguration configuration, string? bank) =>
 {
-    var token = JsonNode.Parse(await _nordigenService.GetAccessToken(httpClientFactory));
-    var redirectUrl = $"{ctx.Request.Scheme}://{ctx.Request.Host}/Transactions";
-    
-    var requisition = await _nordigenService.GetRequisition(httpClientFactory, token?["access"]?.ToString()!, redirectUrl, bank!);
+    var token = JsonNode.Parse(await GetAccessToken(httpClientFactory, configuration));
+    var redirect = $"{ctx.Request.Scheme}://{ctx.Request.Host}/Transactions";
+    using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/requisitions/");
+
+    request.Content = JsonContent.Create(new { redirect, institution_id = bank, user_language = "EN" });
+    request.Headers.Add("Authorization", $"Bearer {token?["access"]!}");
+
+    var requisition = await (await httpClientFactory.CreateClient().SendAsync(request)).Content.ReadAsStringAsync();
     ctx.Response.Redirect(JsonNode.Parse(requisition)?["link"]?.ToString()!);
 });
 
@@ -57,6 +61,19 @@ static async Task<string> Get(string url, string access_token, IHttpClientFactor
 {
     using var request = new HttpRequestMessage(HttpMethod.Get, $"https://ob.nordigen.com/api/v2/{url}");
     request.Headers.Add("Authorization", $"Bearer {access_token}");
+
+    return await (await httpClientFactory.CreateClient().SendAsync(request)).Content.ReadAsStringAsync();
+}
+
+static async Task<string> GetAccessToken(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+{
+    using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/token/new/");
+
+    request.Content = JsonContent.Create(new
+    {
+        secret_id = configuration["Nordigen:secret_id"],
+        secret_key = configuration["Nordigen:secret_key"]
+    });
 
     return await (await httpClientFactory.CreateClient().SendAsync(request)).Content.ReadAsStringAsync();
 }

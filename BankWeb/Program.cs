03ac36302c1b3f6f4671756e4b27886cf57e598f
Olav Salhus <osa@live.no>
2022-07-14 13:00:27 +0200

    Refactor use minimal api and razor pages (no controller)

--- BankWeb/Program.cs
+++ BankWeb/Program.cs
@@ -1,16 +1,17 @@
+using System.Text.Json.Nodes;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient<BankWeb.Controllers.HomeController>();
-builder.Services.AddSingleton<BankWeb.Services.NordigenService>();
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<NordigenService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -18,12 +19,57 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+app.MapGet("GetAccessToken", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService) =>
+    await ctx.Response.WriteAsync(await _nordigenService.GetAccessToken(httpClientFactory.CreateClient()))
+);
+
+app.MapGet("BankLogin", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService, string? bank) =>
+{
+    var httpClient = httpClientFactory.CreateClient();
+    var token = JsonNode.Parse(await _nordigenService.GetAccessToken(httpClient));
+    var redirectUrl = $"{ctx.Request.Scheme}://{ctx.Request.Host}/Transactions";
+
+    var requisition = await _nordigenService.GetRequisition(httpClient, token["access"].ToString(), redirectUrl, bank);
+    ctx.Response.Redirect(JsonNode.Parse(requisition)["link"].ToString());
+});
+
+app.MapGet("GetBanks", async (HttpContext ctx, IHttpClientFactory httpClientFactory, NordigenService _nordigenService, string? country, string? access_token) =>
+    await ctx.Response.WriteAsync(await Get($"institutions/?country={country}", access_token, httpClientFactory))
+);
+
+app.MapGet("ListBankAccounts", async (HttpContext ctx, IHttpClientFactory httpClientFactory, string? reference, string? access_token) =>
+{
+    var accounts = JsonNode.Parse(await Get($"requisitions/{reference}/", access_token, httpClientFactory))["accounts"].AsArray();
+
+    var accountObjects = await Task.WhenAll(
+        accounts.Select(async accountId =>
+        {
+            string metadata = await Get($"accounts/{accountId}/", access_token, httpClientFactory);
+            //string details = await Get($"accounts/{accountId}/details/", access_token, httpClientFactory); @"""details"": {details},"
+            string balances = (await Get($"accounts/{accountId}/balances/", access_token, httpClientFactory)).Trim()[1..^1];
+            string transactions = (await Get($"accounts/{accountId}/transactions/", access_token, httpClientFactory)).Trim()[1..^1];
+            return @$"
+                {{
+                    ""id"": ""{accountId}"", 
+                    ""metadata"": {metadata},
+                    {balances},
+                    {transactions}
+                }}";
+        })
+    );
+
+    await ctx.Response.WriteAsync(@$"[{string.Join(", ", accountObjects)}]");
+});
+
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
+
+static async Task<string> Get(string url, string access_token, IHttpClientFactory httpClientFactory)
+{
+    using var request = new HttpRequestMessage(HttpMethod.Get, $"https://ob.nordigen.com/api/v2/{url}");
+    request.Headers.Add("Authorization", $"Bearer {access_token}");
+
+    return await (await httpClientFactory.CreateClient().SendAsync(request)).Content.ReadAsStringAsync();
+}
--- BankWeb/Services/NordigenService.cs
+++ BankWeb/Services/NordigenService.cs
@@ -1,34 +1,30 @@
-﻿using System.Net.Http.Headers;
-
-namespace BankWeb.Services
+﻿    public class NordigenService
 {
-    public class NordigenService
+    private readonly IConfiguration Configuration;
+    public NordigenService(IConfiguration configuration)
     {
-        private readonly IConfiguration Configuration;
-        public NordigenService(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        public async Task<string> GetAccessToken(HttpClient httpClient)
-        {
-            using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/token/new/");
+        Configuration = configuration;
+    }
+    public async Task<string> GetAccessToken(HttpClient httpClient)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/token/new/");
 
-            request.Content = JsonContent.Create(new {
-                secret_id = Configuration["Nordigen:secret_id"],
-                secret_key = Configuration["Nordigen:secret_key"]
-            });
+        request.Content = JsonContent.Create(new {
+            secret_id = Configuration["Nordigen:secret_id"],
+            secret_key = Configuration["Nordigen:secret_key"]
+        });
 
-            return await (await httpClient.SendAsync(request)).Content.ReadAsStringAsync();
-        }
+        return await (await httpClient.SendAsync(request)).Content.ReadAsStringAsync();
+    }
 
-        public async Task<string> GetRequisition(HttpClient httpClient, string access_token, string redirect, string institution_id)
-        {
-            using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/requisitions/");
+    public async Task<string> GetRequisition(HttpClient httpClient, string access_token, string redirect, string institution_id)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Post, "https://ob.nordigen.com/api/v2/requisitions/");
 
-            request.Content = JsonContent.Create(new { redirect, institution_id, user_language = "EN" });
-            request.Headers.Add("Authorization", $"Bearer {access_token}");
+        request.Content = JsonContent.Create(new { redirect, institution_id, user_language = "EN" });
+        request.Headers.Add("Authorization", $"Bearer {access_token}");
 
-            return await (await httpClient.SendAsync(request)).Content.ReadAsStringAsync();
-        }
+        return await (await httpClient.SendAsync(request)).Content.ReadAsStringAsync();
     }
 }
+
